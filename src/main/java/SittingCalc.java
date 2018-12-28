59e14be5b4e62b2fe6251bd4a337314db81dbe5e
KevinCobb <bender953@gmail.com>
2018-12-27 20:14:05 -0500

    add Family A

--- src/main/java/SittingCalc.java
+++ src/main/java/SittingCalc.java
@@ -3,6 +3,9 @@ public class SittingCalc {
 	
 	static double startTime;
 	static double endTime;
+	private final static int dayTimeRateFamA = 15;
+	private final static int afterBedTimeRateFamA = 20;
+	private final static int bedTimeA = 23;
 
 	public SittingCalc(double startTime, double endTime) {
 		this.startTime = Math.round(startTime);
@@ -13,8 +16,25 @@ public class SittingCalc {
 		return startTime >= 17 && endTime <= 28;
 	}
 
-	
+	public int calculatePayFamA() {
+		int totalPayA = 0;
+		for (double currentHour = startTime; currentHour < endTime; currentHour ++) {
+			if (beforeBedTimeA(currentHour)) {
+				totalPayA += dayTimeRateFamA;
+			} else if (afterBedTimeFamA(currentHour)) {
+				totalPayA += afterBedTimeRateFamA ;
+			}
+		}
+		return totalPayA;
+	}
 
+	private static boolean afterBedTimeFamA(double currentHour) {
+		return currentHour > bedTimeA;
+	}
+
+	private static boolean beforeBedTimeA(double currentHour) {
+		return currentHour < bedTimeA;
+	}
 	
 	
 }
--- src/test/java/SittingCalcTest.java
+++ src/test/java/SittingCalcTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -15,4 +16,17 @@ public class SittingCalcTest {
 		SittingCalc(5, 10);
 		assertEquals(false, SittingCalc.timeValid());
 			}
+	
+	@Test
+	public void whenStartTimeIsEnteredItIsNowAValidTime() {
+		SittingCalc(17, 21);
+		assertEquals(true, SittingCalc.timeValid());
+		
+	}
+	
+	@Test 
+	public void dayPayRateForFamilyAShouldBe15() {
+	    SittingCalc(17, 18);
+	    assertEquals(15, time.calculatePayFamA());
+	}
 }
